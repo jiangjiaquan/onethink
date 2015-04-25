8a42c0283219ca41a082b467fd4f021130021775
jiangjiaquan <540747521@qq.com>
2015-04-26 00:09:05 +0800

    修改添加三个引导文件的作者注释信息
    修改添加三个引导文件的作者注释信息

--- wwwroot/WEB_DIR_GUIDE/index.php
+++ wwwroot/WEB_DIR_GUIDE/index.php
@@ -2,15 +2,13 @@
 // +----------------------------------------------------------------------
 // | OneThink [ WE CAN DO IT JUST THINK IT ]
 // +----------------------------------------------------------------------
-// | Copyright (c) 2013 http://www.onethink.cn All rights reserved.
-// +----------------------------------------------------------------------
-// | Author: 麦当苗儿 <zuojiazi@vip.qq.com> <http://www.zjzit.cn>
+// | Author: jiangjiaquan <540747521@qq.com> <http://www.zjzit.cn>
 // +----------------------------------------------------------------------
 
 if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
 $domain = $_SERVER ['HTTP_HOST']; // < 判断是从哪个域名访问过来
 define('__ROOT__','./'.$domain);
 
-define('PROJECT_PATH', '../myfolder/'.$domain.'/');
+define('PROJECT_PATH', '../myfolder/'.$domain.'/'); // < myfolder 此目录自行定义
 
 require PROJECT_PATH.'index.php';
