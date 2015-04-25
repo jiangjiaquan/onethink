0f3b64859d43f2fa645d155a7478b1c204dd02b1
jiangjiaquan <540747521@qq.com>
2015-04-25 23:59:46 +0800

    新增index.php install.php admin.php 三个文件
    增加文件夹 WEB_DIR_GUIDE 存放WEB 目录三个引导文件

---
+++ wwwroot/WEB_DIR_GUIDE/install.php
@@ -0,0 +1,16 @@
+<?php
+// +----------------------------------------------------------------------
+// | OneThink [ WE CAN DO IT JUST THINK IT ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2013 http://www.onethink.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: 麦当苗儿 <zuojiazi@vip.qq.com> <http://www.zjzit.cn>
+// +----------------------------------------------------------------------
+
+if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
+$domain = $_SERVER ['HTTP_HOST']; // < 判断是从哪个域名访问过来
+define('__ROOT__','./'.$domain);
+
+define('PROJECT_PATH', '../myfolder/'.$domain.'/');
+
+require PROJECT_PATH.'install.php';
