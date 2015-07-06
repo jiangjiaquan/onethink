f14f6ba43eaf3c829ec45d61c815e116119590de
jiangjiaquan <540747521@qq.com>
2015-07-06 16:57:46 +0800

    增加后台控制器读取图片的操作

--- wwwroot/Application/Admin/Controller/FileController.class.php
+++ wwwroot/Application/Admin/Controller/FileController.class.php
@@ -84,4 +84,26 @@ class FileController extends AdminController {
         /* 返回JSON数据 */
         $this->ajaxReturn($return);
     }
+    
+    /**
+      * 读取图片
+      * @author jiangjiaquan <540747521@qq.com>
+      */
+    public function readFile( $filename = ''){
+	$filename = I('fullpath');
+	// 限制只能读取Uploads目录下的文件
+	$filename = PROJECT_PATH.'Uploads/'.$filename;
+	// 取图片类型
+	$type = end(preg_split('/\./', $filename));
+	if(is_file($filename)){
+		header('Content-Length:'.filesize($filename));
+		header('Content-Type:image/'.$type);
+		readfile($filename);
+	}else{
+		send_http_status(404);
+// 		header('Content-Length:'.filesize($filename));
+// 		header('Content-Type:image/'.$type);
+// 		readfile('no.jpg');
+	}
+    }
 }
